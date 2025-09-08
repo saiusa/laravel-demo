3cb61ca23755c4d96d2ad1288c025914f316d8be
titoabz <abby.auxtero@urios.edu.ph>
2025-09-08 11:33:53 +0800

    new

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello with Effects</title>
+</head>
+<body>
+    <?php
+        echo "<h1>BORAHAE<3<h1>";
+    ?>
+</body>
+</html>
